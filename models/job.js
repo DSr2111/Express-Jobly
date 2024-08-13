1ee49292568db70467368ed1cebe78b2956fd97c
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-13 07:55:37 -0400

    Job class start

--- models/job.js
+++ models/job.js
@@ -3,3 +3,16 @@
 const db = require("../db");
 const { NotFoundError } = require("../expressError");
 const { sqlForPartialUpdate } = require("../helpers/sql");
+const { findAll } = require("./company");
+
+class Job {
+  static async create(data) {}
+
+  static async findAll({ minSalary, hasEquity, title } = {}) {}
+
+  static async get(id) {}
+
+  static async update(id) {}
+
+  static async remove(id) {}
+}
