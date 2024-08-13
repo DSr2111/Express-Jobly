405d064844cc4fee2b83533b445de166ea690cd4
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-13 08:32:28 -0400

    create and findAll

--- models/job.js
+++ models/job.js
@@ -6,9 +6,60 @@ const { sqlForPartialUpdate } = require("../helpers/sql");
 const { findAll } = require("./company");
 
 class Job {
-  static async create(data) {}
+  static async create(data) {
+    const result = await db.query(
+      `INSERT INTO jobs (title,
+                                 salary,
+                                 equity,
+                                 company_handle)
+               VALUES ($1, $2, $3, $4)
+               RETURNING id, title, salary, equity, company_handle AS "companyHandle"`,
+      [data.title, data.salary, data.equity, data.companyHandle]
+    );
+    let job = result.rows[0];
 
-  static async findAll({ minSalary, hasEquity, title } = {}) {}
+    return job;
+  }
+
+  static async findAll({ minSalary, hasEquity, title } = {}) {
+    let query = `SELECT j.id,
+                        j.title,
+                        j.salary,
+                        j.equity,
+                        j.company_handle AS "companyHandle",
+                        c.name AS "companyName"
+                 FROM jobs j 
+                   LEFT JOIN companies AS c ON c.handle = j.company_handle`;
+    let whereExpressions = [];
+    let queryValues = [];
+
+    // For each possible search term, add to whereExpressions and
+    // queryValues so we can generate the right SQL
+
+    if (minSalary !== undefined) {
+      queryValues.push(minSalary);
+      whereExpressions.push(`salary >= $${queryValues.length}`);
+    }
+
+    if (hasEquity === true) {
+      whereExpressions.push(`equity > 0`);
+    }
+
+    if (title !== undefined) {
+      queryValues.push(`%${title}%`);
+      whereExpressions.push(`title ILIKE $${queryValues.length}`);
+    }
+
+    if (whereExpressions.length > 0) {
+      query += " WHERE " + whereExpressions.join(" AND ");
+    }
+
+    // Finalize query and return results
+
+    query += " ORDER BY title";
+    const jobsRes = await db.query(query, queryValues);
+    return jobsRes.rows;
+  }
 
   static async get(id) {}
 
