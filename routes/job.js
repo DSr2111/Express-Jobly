a1f0d7d800c8b2a4cc20e1e5975ab003dd7fa0bc
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-16 06:38:13 -0400

    routes start

--- routes/job.js
+++ routes/job.js
@@ -5,3 +5,23 @@
 const express = require("express");
 const Job = require("../models/job");
 
+router.post("/", async function (req, res, next) {
+  try {
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.get("/", async function (req, res, next) {
+  try {
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.patch("/:id", async function (req, res, next) {
+  try {
+  } catch (err) {
+    return next(err);
+  }
+});
